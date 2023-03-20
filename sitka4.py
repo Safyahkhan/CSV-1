52033c00cb309f2acbb75076bd1a71d27f5dbe92
SafyahKhan <safyah_khan@gmail.comm>
2023-03-20 00:24:21 -0400

    last

--- sitka4.py
+++ sitka4.py
@@ -50,6 +50,7 @@ plt.xlabel("Dates", fontsize=12)
 plt.ylabel("Temperature(F)", fontsize=12)
 plt.tick_params(axis= 'both', which='major', labelsize=12)
 
+# Rotate the x-axis labels
 fig.autofmt_xdate()
 
 plt.show()
